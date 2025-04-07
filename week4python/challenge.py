e3e0fa8d77e17d06c5ed8a3c72ae3e15fbb29e3a
tutich <pkoinange02@gmail.com>
2025-04-08 02:16:35 +0300

    week4 challenge

---
+++ week4python/challenge.py
@@ -0,0 +1,18 @@
+
+
+
+#  Challenge: Create a program that reads a text file, processes its content, and writes the results to a new file.
+
+with open("input.txt", "r") as file:
+    data = file.read()
+    print(data)
+
+word_count = len(data.split())
+
+word_upper = data.upper()
+    
+with open("output.txt", "w") as file:
+    file.write(word_upper)
+    file.write("\nWord count: " + str(word_count))
+
+print("\nFile successfully created.\n")
